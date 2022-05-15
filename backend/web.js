235a03b5517226959d2a8bcbbd07b0413f858c7f
rstx32 <rstx32@gmail.com>
2022-05-15 15:15:07 +0700

    add routes

--- backend/web.js
+++ backend/web.js
@@ -5,6 +5,16 @@ import passport from 'passport'
 import session from 'express-session'
 import connectEnsureLogin from 'connect-ensure-login'
 import flash from 'connect-flash'
+import xlsx from 'xlsx'
+import { getCandidates, getVoters, importVoters } from './getAPI.js'
+import {
+  getValidator,
+  voterValidate,
+  isVoterValidated,
+  isCandidateValidated,
+  candidateValidate,
+} from './db.js'
+import User from './model/user.js'
 dotenv.config({ path: 'backend/config/.env' })
 
 const app = express()
@@ -23,10 +33,6 @@ const app = express()
   .use(passport.session())
   .use(flash())
 
-import { getCandidates, getVoters } from './getAPI.js'
-import { getValidator } from './db.js'
-import User from './user.js'
-
 passport.use(User.createStrategy())
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
@@ -39,7 +45,7 @@ app.get('/login', (req, res) => {
   res.render('auth/login', {
     layout: 'auth/login',
     title: 'login',
-    flashMessage: {errorMessage,successMessage},
+    flashMessage: { errorMessage, successMessage },
   })
 })
 
@@ -57,10 +63,18 @@ app.post(
 )
 // end login page
 
+// logout
+app.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/login')
+})
+
+// root page
 app.get('/', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
   res.redirect('/voters')
 })
 
+// voters
 app.get('/voters', connectEnsureLogin.ensureLoggedIn(), async (req, res) => {
   // if query is empty, then add default query
   if (Object.keys(req.query).length === 0) {
@@ -70,26 +84,68 @@ app.get('/voters', connectEnsureLogin.ensureLoggedIn(), async (req, res) => {
     }
   }
 
+  const validator = req.user._id
   const voters = await getVoters(req.query)
+  const isValidated = await isVoterValidated(req.user._id)
+
   res.render('voters', {
     layout: 'layouts/main-layout',
     title: 'voters',
     voters,
+    validator,
+    isValidated,
   })
 })
 
-app.get('/candidates', connectEnsureLogin.ensureLoggedIn(), async (req, res) => {
-  const candidates = await getCandidates()
-  const candidatesCount = candidates.length
-
-  res.render('candidates', {
-    layout: 'layouts/main-layout',
-    title: 'candidates',
-    candidates,
-    candidatesCount,
-  })
+app.post('/voters', async (req, res) => {
+  await voterValidate(req.body.voters)
+  res.redirect('/validator')
 })
 
+app.get('/download-voters', async (req, res) => {
+  const voters = await importVoters()
+  const fix = voters.docs
+  fix.forEach((v) => {
+    delete v._id
+    delete v.__v
+  })
+
+  const worksheet = xlsx.utils.json_to_sheet(fix)
+  const workbook = xlsx.utils.book_new()
+
+  xlsx.utils.book_append_sheet(workbook, worksheet, 'Sheet 1')
+  xlsx.writeFile(workbook, 'sample.xlsx')
+})
+// end voters
+
+// candidates
+app.get(
+  '/candidates',
+  connectEnsureLogin.ensureLoggedIn(),
+  async (req, res) => {
+    const candidates = await getCandidates()
+    const candidatesCount = candidates.length
+    const validator = req.user._id
+    const isValidated = await isCandidateValidated(req.user._id)
+
+    res.render('candidates', {
+      layout: 'layouts/main-layout',
+      title: 'candidates',
+      candidates,
+      candidatesCount,
+      validator,
+      isValidated,
+    })
+  }
+)
+
+app.post('/candidates', async (req, res) => {
+  await candidateValidate(req.body.candidates)
+  res.redirect('/validator')
+})
+// end candidates
+
+// validator
 app.get('/validator', async (req, res) => {
   const validator = await getValidator()
   const validatorCount = validator.length
@@ -98,7 +154,7 @@ app.get('/validator', async (req, res) => {
     layout: 'layouts/main-layout',
     title: 'validator',
     validator,
-    validatorCount
+    validatorCount,
   })
 })
 
