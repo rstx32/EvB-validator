ccdd92ff6f8da9a0794e0e68b9202dd2e15e2bed
rstx32 <rstx32@gmail.com>
2022-05-27 17:56:24 +0700

    add some methods

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,10 @@
 import Validator from './model/validator.js'
-import { Voter, VoterFix } from './model/voter.js'
-import { Candidate, CandidateFix } from './model/candidate.js'
+import { Voter } from './model/voter.js'
+import { Admin } from './model/admin.js'
+import { Complaint } from './model/complaint.js'
+import jsonwebtoken from 'jsonwebtoken'
+import nodemailer from 'nodemailer'
+import ejs from 'ejs'
 
 const getValidator = async () => {
   return await Validator.find()
@@ -34,9 +38,9 @@ const validate = async (data, type) => {
       },
       {
         $set: {
-          voterStatus: validation,
-          voterReason: reason,
-          voterSolve: voterSolve,
+          'voter.status': validation,
+          'voter.reason': reason,
+          'voter.solve': voterSolve,
         },
       }
     )
@@ -47,65 +51,120 @@ const validate = async (data, type) => {
       },
       {
         $set: {
-          candidateStatus: validation,
-          candidateReason: reason,
-          candidateSolve: candidateSolve,
+          'candidate.status': validation,
+          'candidate.reason': reason,
+          'candidate.solve': candidateSolve,
         },
       }
     )
   }
 
+  isValidatorValidated()
+}
+
+// check if all validator is validated
+const isValidatorValidated = async () => {
   const validators = await getValidator()
-  let voterCount,
+  let voterCount = 0,
     candidateCount = 0
   for (let index = 0; index < validators.length; index++) {
-    if (validators[index].voterStatus === 'valid') {
+    if (validators[index].voter.status === 'valid') {
       voterCount++
-    } else if (validators[index].candidateStatus === 'valid') {
+    }
+    if (validators[index].candidate.status === 'valid') {
       candidateCount++
     }
   }
 
   if (voterCount === validators.length) {
-    migrate('voter')
-  } else if (candidateCount === validators.length) {
-    migrate('candidate')
+    lockAdmin('voter')
+    sendEmail()
+  }
+  if (candidateCount === validators.length) {
+    lockAdmin('candidate')
   }
 }
 
-// migrate fixed voter & candidate
-const migrate = async (type) => {
+// lock admin from CRUD
+const lockAdmin = async (type) => {
   if (type === 'voter') {
-    const voters = await Voter.find()
-    for (let index = 0; index < voters.length; index++) {
-      console.log(voters[index])
-      await VoterFix.create({
-        nim: voters[index].nim,
-        fullname: voters[index].fullname,
-        email: voters[index].email,
-        password: voters[index].password,
-        public_key: voters[index].public_key,
-        photo: voters[index].photo,
-      })
-    }
+    await Admin.updateOne(
+      {
+        username: 'admin',
+      },
+      {
+        $set: {
+          voterAccess: 'deny',
+        },
+      }
+    )
   } else if (type === 'candidate') {
-    const candidates = await Candidate.find()
-    for (let index = 0; index < candidates.length; index++) {
-      await CandidateFix.create({
-        candidate: candidates[index].candidate,
-        viceCandidate: candidates[index].viceCandidate,
-        photo: candidates[index].photo,
-      })
-    }
+    await Admin.updateOne(
+      {
+        username: 'admin',
+      },
+      {
+        $set: {
+          candidateAccess: 'deny',
+        },
+      }
+    )
   }
 }
-// migrate('voter')
+
+// send email to voter
+const sendEmail = async () => {
+  const gmail = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'xtsr23@gmail.com',
+      pass: 'Mas.ganteng@32',
+    },
+  })
+
+  const mailtrap = nodemailer.createTransport({
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+      user: '4e56d2a27d9572',
+      pass: '81878f80487ec9',
+    },
+  })
+
+  const voters = await Voter.find()
+  const extractValue = (arr, prop) => {
+    let extractedValue = arr.map((item) => item[prop])
+    return extractedValue
+  }
+  const voterEmail = extractValue(voters, 'email')
+
+  const sendBulkEmail = (interval) => {
+    setTimeout(async () => {
+      const fileHTML = await ejs.renderFile('views/email.ejs', {
+        voter: voters[interval].fullname,
+        key: voters[interval].key,
+      })
+      const mailOptions = {
+        from: 'evb-organizer@evb.com',
+        to: voterEmail[interval],
+        subject: 'Voter Registration',
+        html: fileHTML,
+      }
+      mailtrap.sendMail(mailOptions)
+      console.log(`send email ${interval}`)
+    }, 2500 * interval)
+  }
+
+  for (let i = 0; i < voters.length; i++) {
+    sendBulkEmail(i)
+  }
+}
 
 // accept solve from admin
 const acceptSolve = async (data, type) => {
   const validator = await getSingleValidator(data.validatorID)
-  let acceptStatus,
-    status,
+  let acceptStatus = '',
+    status = '',
     reason = ''
   if (data.acceptStatus === 'true') {
     acceptStatus = 'accept'
@@ -114,8 +173,8 @@ const acceptSolve = async (data, type) => {
   } else if (data.acceptStatus === 'false') {
     acceptStatus = 'reject'
     status = 'invalid'
-    if (type === 'voter') reason = validator.voterReason
-    else if (type === 'candidate') reason = validator.candidateReason
+    if (type === 'voter') reason = validator.voter.reason
+    else if (type === 'candidate') reason = validator.candidate.reason
   }
 
   if (validator !== null) {
@@ -123,18 +182,18 @@ const acceptSolve = async (data, type) => {
       await Validator.updateOne(
         { _id: validator.id },
         {
-          voterSolve: acceptStatus,
-          voterReason: reason,
-          voterStatus: status,
+          'voter.solve': acceptStatus,
+          'voter.reason': reason,
+          'voter.status': status,
         }
       )
     } else if (type === 'candidate') {
       await Validator.updateOne(
         { _id: validator.id },
         {
-          candidateSolve: acceptStatus,
-          candidateReason: reason,
-          candidateStatus: status,
+          'candidate.solve': acceptStatus,
+          'candidate.reason': reason,
+          'candidate.status': status,
         }
       )
     }
@@ -143,4 +202,34 @@ const acceptSolve = async (data, type) => {
   }
 }
 
-export { getValidator, getSingleValidator, validate, acceptSolve }
+// generate JWT for validator
+const generateJWT = async (username) => {
+  const token = jsonwebtoken.sign({ username: username }, process.env.JWT, {
+    expiresIn: '7d',
+  })
+
+  return await Validator.updateOne(
+    { username: username },
+    {
+      $set: {
+        token: token,
+      },
+    }
+  )
+}
+
+// check if complaint is still exist
+const checkComplaint = async () => {
+  return await Complaint.find({
+    status: 'unsolved',
+  })
+}
+
+export {
+  getValidator,
+  getSingleValidator,
+  validate,
+  acceptSolve,
+  generateJWT,
+  checkComplaint,
+}
