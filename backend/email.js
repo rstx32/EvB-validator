37965ddb08b9a68dc3babe4399777279d6e3007e
rstx32 <rstx32@gmail.com>
2022-06-07 17:19:39 +0700

    mailtrap auth to env

--- backend/email.js
+++ backend/email.js
@@ -12,8 +12,8 @@ const mailtrap = nodemailer.createTransport({
   host: 'smtp.mailtrap.io',
   port: 2525,
   auth: {
-    user: '4e56d2a27d9572',
-    pass: '81878f80487ec9',
+    user: process.env.USERNAME_MAILTRAP,
+    pass: process.env.PASSWORD_MAILTRAP,
   },
 })
 
