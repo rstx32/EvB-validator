0d3aa4849b51aac857b910171f77b93aef6620d5
Restu Indera <rstx32@gmail.com>
2022-06-12 01:19:21 +0700

    add ethereal transporter

--- backend/email.js
+++ backend/email.js
@@ -12,9 +12,18 @@ const mailtrap = nodemailer.createTransport({
   host: 'smtp.mailtrap.io',
   port: 2525,
   auth: {
-    user: process.env.USERNAME_MAILTRAP,
-    pass: process.env.PASSWORD_MAILTRAP,
+    user: process.env.USERNAME_EMAIL,
+    pass: process.env.PASSWORD_EMAIL,
   },
 })
 
-export { gmail, mailtrap }
+const ethereal = nodemailer.createTransport({
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+    user: process.env.USERNAME_EMAIL,
+    pass: process.env.PASSWORD_EMAIL,
+  },
+})
+
+export { gmail, mailtrap, ethereal }
